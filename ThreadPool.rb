c39c13d18d603950f5ec47213e5aa7f1a6c00117
Daniel Bush <dlb.id.au@gmail.com>
2009-10-11 20:39:20 +1100

    Added licensing to code.

--- ThreadPool.rb
+++ ThreadPool.rb
@@ -1,3 +1,14 @@
+#  
+#  This file is part of ThreadPool, a jruby or ruby-based
+#  thread pool manager.
+#  Copyright (C) 2009 Daniel Bush
+#  This program is distributed under the terms of the GNU
+#  General Public License.  A copy of the license should be
+#  enclosed with this project in the file LICENSE.  If not
+#  see <http://www.gnu.org/licenses/>.
+# 
+#
+
 class ThreadPool
   require 'thread'
 
