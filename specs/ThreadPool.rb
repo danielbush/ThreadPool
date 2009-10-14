8cf87fa34933de6d81b015c17474a05e3d771d01
Daniel Bush <dlb.id.au@gmail.com>
2009-10-14 19:57:47 +1100

    Added spec file for ThreadPool.

---
+++ specs/ThreadPool.rb
@@ -0,0 +1,19 @@
+require '../lib/ThreadPool'
+
+TP=ThreadPool
+
+describe ThreadPool do
+
+  describe "dispatching lambdas" do
+
+    it "should work with no arguments" do
+      result=nil
+      tp = TP.new(1)
+      tp.dispatch lambda {result='hi'}
+      tp.join
+      result.should == 'hi'
+    end
+
+  end
+
+end
